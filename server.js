dd89c8f59f957263ad4ad1e5d0fc852dc13fa822
andrewshevtsov <andrewshevtsov@inbox.ru>
2020-06-02 20:58:43 +0300

    update server.js

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const path = require('path')
 const port = process.env.PORT || 8080;
 
 const app = express()
-app.use(favicon(__dirname + '/build/favicon.ico'))
 
 app.use(express.static(__dirname))
 app.use(express.static(path.join(__dirname + '/build')))
+app.use(favicon(__dirname + '/build/favicon.ico'))
 
 app.get('/ping', function (req, res) {
     return res.send('pong')
